b28a483aa5425a54c2336a4198a92a3053125d3f
inaimathi <inaimathi@self>
2015-09-18 18:33:22 -0400

    - const-ish? now checks if the given const string is a prefix of the given number (and returns true if that's the case; it's equal to the constant to the finest tolerance we can check against, which is good enough) - define separate pronounce-int that handles the "too-long-to-be-showin-in-english" error, and returns something in the event of one (currently, that something is the string "a fuckton", which we should do better than) - define separate pronounce-ratio which uses pronounce-int in place of a direct ~r call to format (so, you can now get a result like "sixteen over a fuckton" or "a fuckton over thirty-seven") - floats use pronounce-int for their integer part - define a fallthrough clause in the magic handler that returns a 400 error if the given parameter matches no other clause

--- cl-pronounce.lisp
+++ cl-pronounce.lisp
@@ -2,21 +2,19 @@
 (in-package #:cl-pronounce)
 
 (define-handler (v1/integer/-num=integer) ()
-  (format nil "~r" num))
+  (pronounce-int num))
 
 (define-handler (v1/ratio/-num=integer/-denom=integer) ()
-  (format nil "~r over ~r" num denom))
+  (pronounce-ratio num denom))
 
 (define-handler (v1/float/-num=string) ()
   (multiple-value-bind (int off) (parse-integer num :junk-allowed t)
-    (format nil "~r point ~{~r~^ ~}"
-	    int (loop for i from (+ off 1) while (> (length num) i)
-		   collect (parse-integer num :start i :end (+ i 1))))))
+    (format nil "~a point ~{~r~^ ~}"
+	    (pronounce-int int) (parse-to-digits num :start (+ off 1)))))
 
 (define-handler (v1/magic/-num=string) ()
   (multiple-value-bind (int off) (parse-integer num :junk-allowed t)
-    (cond ((= (length num) off)
-	   (format nil "~r" int))
+    (cond ((= (length num) off) (pronounce-int int))
 	  ((and (eql #\. (char num off))
 		(const-ish? num +pi-str+))
 	   (const num "pi"))
@@ -24,12 +22,29 @@
 		(const-ish? num +tau-str+))
 	   (const num "tau"))
 	  ((eql #\. (char num off))
-	   (format nil "~r point ~{~r~^ ~}"
-		   int (loop for i from (+ off 1) while (> (length num) i)
-			  collect (parse-integer num :start i :end (+ i 1))))))))
+	   (format nil "~a point ~{~r~^ ~}"
+		   (pronounce-int int) (parse-to-digits num :start (+ off 1))))
+	  (t (set-response-code! 400)
+	     "Shrug. I dunno what that is."))))
 
 (define-handler (v1/magic/-num=integer/-denom=integer) ()
-  (format nil "~r over ~r" num denom))
+  (pronounce-ratio num denom))
+
+(defun parse-to-digits (num-str &key start)
+  (loop for i from start while (> (length num-str) i)
+     collect (parse-integer num-str :start i :end (+ i 1))))
+
+(defun pronounce-ratio (num denom)
+  (concatenate
+   'string (pronounce-int num)
+   " over " (pronounce-int denom)))
+
+(defun pronounce-int (num)
+  (handler-case
+      (format nil "~r" num)
+    (simple-error ()
+      ;; TODO - a reasonable value here
+      "a fuckton")))
 
 (defun start (&key (port 5000))
   (woo:run (make-app) :port port))
